1c2a22c4c00c8c1af8bb0183404bae0327b36b94
Meron <malemu17@yahoo.com>
2019-01-31 20:13:14 -0800

    push

---
+++ project-directory/js/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var eyecolor = prompt("Is Meron's eye color blue ?" +'please answer yes or no').toLowerCase;
+            console.log(eyecolor);
+var channel = prompt('Does Meron likes Facebook channel?'+ 'please answer yes or no').toLowerCase;
+            console.log(channel)
+var football = prompt('Does Meron likes football?'+ 'please answer yes or no').toLowerCase;
+            console.log(football)
+var life = prompt('Do I like my life?'+ 'please answer yes or no' ).toLowerCase;
+            console.log(life)
+
+var jokes= prompt ('Do I like jokes?'+ 'please answer yes or no').toLowerCase;
+            console.log(jokes)
