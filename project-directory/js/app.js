9a39cc11420182a79559367abaa65056a7b1d518
Meron <malemu17@yahoo.com>
2019-02-02 14:14:43 -0800

    push

--- project-directory/js/app.js
+++ project-directory/js/app.js
@@ -1,13 +1,35 @@
-'use strict'
+'use strict';
 
-var eyecolor = prompt("Is Meron's eye color blue ?" +'please answer yes or no').toLowerCase;
+var eyecolor = prompt('Is Meron\'s eye color blue ? please answer yes or no').toLowerCase();
             console.log(eyecolor);
-var channel = prompt('Does Meron likes Facebook channel?'+ 'please answer yes or no').toLowerCase;
-            console.log(channel)
-var football = prompt('Does Meron likes football?'+ 'please answer yes or no').toLowerCase;
-            console.log(football)
-var life = prompt('Do I like my life?'+ 'please answer yes or no' ).toLowerCase;
-            console.log(life)
+           
+                
 
-var jokes= prompt ('Do I like jokes?'+ 'please answer yes or no').toLowerCase;
+var channel = prompt('Does Meron likes Facebook channel?'+ 'please answer yes or no').toLowerCase();
+            console.log(channel)
+            
+                
+
+var football = prompt('Does Meron likes football?'+ 'please answer yes or no').toLowerCase();
+            console.log(football)
+            
+                
+
+var life = prompt('Do I like my life?'+ 'please answer yes or no').toLowerCase();
+            console.log(life)
+            
+                
+
+var jokes= prompt ('Do I like jokes?'+ 'please answer yes or no').toLowerCase();
             console.log(jokes)
+           
+                
+
+
+if (eyecolor==='no'&& channel ==='yes'&& football ==='no'&& life ==='yes'&& jokes ==='yes'){
+            alert('Perfect!!! you know me more than I can imagine');
+        }
+        else {
+
+            alert('No worries, you will have enough time to know me more');
+        }
